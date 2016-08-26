dc30abe9b1d3a34ab42ba7bbb6c1a11c6d5aa9e2
kaitj <jasonkai7@gmail.com>
2016-08-26 16:13:10 -0400

    Update gen_subjids.py
    Added the subject list generation as a Nipype Node

--- Data_management/gen_subjids.py
+++ Data_management/gen_subjids.py
@@ -1,6 +1,6 @@
 """
 Created on Aug. 24, 2016
-Upated on Aug. 25, 2016
+Upated on Aug. 26, 2016
 
 Generate array with subjids to be passed onto datagrabber
 NOTE: Directories are all hard coded
@@ -12,6 +12,7 @@ NOTE: Directories are all hard coded
 # Import
 ########
 import os, getpass # Directory & user library
+from nipype import IdentityInterface, Node
 
 # Global Variables
 user = getpass.getuser() # Grabs username of user currently logged on
@@ -24,8 +25,14 @@ listids = os.listdir(base_dir) # Store the contents of the path
 sort_listids = sorted(listids) # Sort contents alphabetically
 
 # Array to store subject ids
-subjids = []
+subjid = []
 
 for subj in range(len(sort_listids)):
     if "EPI_" in sort_listids[subj]:
-        subjids.append(sort_listids[subj])
+        subjid.append(sort_listids[subj])
+        
+##############################
+# Iterable list of subject ids
+##############################
+node_subjid = Node(IdentityInterface(fields=['subject_id']),name="subjid")
+node_subjid.iterables = [('subject_id', subjid)]
